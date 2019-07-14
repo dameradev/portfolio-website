4cde7ebbb0e12499d63fe2c23fd8c438193309d1
=DamjanRadev <radevdamjan@gmail.com>
2019-07-14 20:57:11 +0200

    Finished mobile and main nav

--- shared.js
+++ shared.js
@@ -1,10 +1,20 @@
 let toggleButton = document.querySelector('.toggle-button');
 let mobile = document.querySelector('.mobile-nav')
 let backdrop = document.querySelector('.backdrop');
+let toggleImage = document.getElementById("toggle-image");
 
 toggleButton.addEventListener('click', ()=> {
-  mobile.classList.add("open");
-  backdrop.classList.add('open');
+  if(!mobile.classList.contains("open")) {
+    mobile.classList.add("open");
+    backdrop.classList.add('open');
+    toggleImage.src = "images/x.png"
+    
+  } else {
+    mobile.classList.remove("open");
+    backdrop.classList.remove('open');
+    toggleImage.src = "images/menu.png"  
+  }
+  
 })
 
 backdrop.addEventListener('click', () => {
